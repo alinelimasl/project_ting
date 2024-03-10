13e6e69256babbf75b7e2e31c06b9aa0c2c37e84
Aline <aline_lima@outlook.com>
2024-03-10 11:08:16 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+from pathlib import Path
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    path = Path(path_file)
+    if not path.suffix.lower() == ".txt":
+        sys.stderr.write("Formato inválido\n")
+        return []
+
+    try:
+        with open(path, "r") as file:
+            news = file.read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path} não encontrado\n")
+        return []
+
+    return news
