f1848fe63a5e964890b55caf2384b316669bf8c4
Aline <aline_lima@outlook.com>
2024-03-10 12:44:42 -0300

    Requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,7 +17,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        sys.stdout.write("Não há elementos\n")
+    else:
+        removed = instance.dequeue()
+        sys.stdout.write(f"Arquivo {removed['nome_do_arquivo']} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
