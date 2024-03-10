1a0e51dca7782edca3f38c46ded725c9ca1d40da
Aline <aline_lima@outlook.com>
2024-03-10 12:31:44 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    new_file = dict(
+        {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": txt_importer(path_file).__len__(),
+            "linhas_do_arquivo": txt_importer(path_file),
+        }
+    )
+
+    if new_file not in instance._fila:
+        instance.enqueue(new_file)
+        sys.stdout.write(str(new_file))
 
 
 def remove(instance):
